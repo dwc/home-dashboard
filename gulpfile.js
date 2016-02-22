4193d3f900422bcce2c04439ed4ffc2cf0e1adb2
Daniel Westermann-Clark <dwc@pobox.com>
2016-02-21 18:19:16 -0800

    Load data CloudWatch and graph via D3.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,41 +1,40 @@
-var gulp = require('gulp');
-var path = require('path');
-var webserver = require('gulp-webserver');
+var gulp = require("gulp");
+var path = require("path");
 
 var PATHS = {
-  SRC: 'src/**/*.js',
-  PUBLIC: 'public/**/*',
-  DEST: 'dist'
+  SRC: "src/**/*.js",
+  PUBLIC: "public/**/*",
+  CONFIGURATION: "configuration/**/*",
+  DEST: "dist"
 };
 
-gulp.task('build-js', function () {
+gulp.task("build-js", function () {
   var files = [
     PATHS.SRC,
-    'node_modules/jquery/dist/jquery.min.js',
-    'node_modules/xively-js/xivelyjs-*.min.js'
+    "node_modules/jquery/dist/jquery.min.js",
+    "node_modules/d3/d3.min.js"
   ];
 
   gulp.src(files)
-    .pipe(gulp.dest(path.join(PATHS.DEST, 'js')));
+    .pipe(gulp.dest(path.join(PATHS.DEST, "js")));
 });
 
-gulp.task('copy-public', function() {
+gulp.task("copy-public", function() {
   gulp.src(PATHS.PUBLIC)
     .pipe(gulp.dest(PATHS.DEST));
 });
 
-gulp.task('build', [ 'build-js', 'copy-public' ]);
-
-gulp.task('watch', function() {
-  gulp.watch([ PATHS.SRC ], [ 'build-js' ]);
-  gulp.watch([ PATHS.PUBLIC ], [ 'copy-public' ]);
+gulp.task("copy-configuration", function() {
+  gulp.src(PATHS.CONFIGURATION)
+    .pipe(gulp.dest(PATHS.DEST));
 });
 
-gulp.task('server', function() {
-  gulp.src(PATHS.DEST)
-    .pipe(webserver({
-      livereload: true
-    }));
+gulp.task("build", [ "build-js", "copy-public", "copy-configuration" ]);
+
+gulp.task("watch", function() {
+  gulp.watch([ PATHS.SRC ], [ "build-js" ]);
+  gulp.watch([ PATHS.PUBLIC ], [ "copy-public" ]);
+  gulp.watch([ PATHS.CONFIGURATION ], [ "copy-configuration" ]);
 });
 
-gulp.task('default', [ 'build', 'watch', 'server' ]);
+gulp.task("default", [ "build", "watch" ]);
--- src/main.js
+++ src/main.js
@@ -1,24 +1,118 @@
-xively.setKey('tFgasBNBO4g3j4HeIUEPrUeQeenFE61B0dPGKoXwosIXzvYr');
-
-$(document).ready(function() {
-  var FEED_ID = '1171698610';
-  var CHANNEL = 'temperature0';
-
-  function convert(celsius) {
+$(document).on("ready", function() {
+  var convertToFahrenheit = function(celsius) {
     return celsius * 1.8 + 32;
-  }
+  };
 
-  function displayData(data) {
-    $('#celsius').text(data.current_value);
-    $('#fahrenheit').text(convert(data.current_value));
-    $('#timestamp').text(data.at);
-  }
+  var renderGraph = function(datapoints) {
+    var margin = {top: 20, right: 20, bottom: 30, left: 50};
+    var width = 960 - margin.left - margin.right;
+    var height = 500 - margin.top - margin.bottom;
 
-  xively.datastream.get(FEED_ID, CHANNEL, function(data) {
-    displayData(data);
-  });
+    var x = d3.time.scale()
+      .range([0, width]);
+    var y = d3.scale.linear()
+      .range([height, 0]);
 
-  xively.datastream.subscribe(FEED_ID, CHANNEL, function(event, data) {
-    displayData(data);
-  });
+    var xAxis = d3.svg.axis()
+      .scale(x)
+      .orient("bottom");
+    var yAxis = d3.svg.axis()
+      .scale(y)
+      .orient("left");
+
+    x.domain(d3.extent(datapoints, function(d) { return d.timestamp; }));
+    y.domain(d3.extent(datapoints, function(d) { return convertToFahrenheit(d.reading); }));
+
+    var line = d3.svg.line()
+      .x(function(d) { return x(d.timestamp); })
+      .y(function(d) { return y(convertToFahrenheit(d.reading)); });
+
+    d3.select("#temperature-graph svg").remove();
+
+    var svg = d3.select("#temperature-graph").append("svg")
+      .attr("width", width + margin.left + margin.right)
+      .attr("height", height + margin.top + margin.bottom)
+      .append("g")
+      .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+    svg.append("g")
+      .attr("class", "x axis")
+      .attr("transform", "translate(0," + height + ")")
+      .call(xAxis);
+    svg.append("g")
+      .attr("class", "y axis")
+      .call(yAxis)
+      .append("text")
+      .attr("transform", "rotate(-90)")
+      .attr("y", 6)
+      .attr("dy", ".71em")
+      .style("text-anchor", "end")
+      .text("Temperature");
+    svg.append("path")
+      .datum(datapoints)
+      .attr("class", "line")
+      .attr("d", line);
+  };
+
+  var showSummary = function(datapoints, precision) {
+    var latest = datapoints[datapoints.length - 1];
+
+    var celsius = latest.reading;
+    $("#temperature-current-celsius").text(celsius.toPrecision(precision));
+
+    var fahrenheit = convertToFahrenheit(latest.reading);
+    $("#temperature-current-fahrenheit").text(fahrenheit.toPrecision(precision));
+
+    $("#temperature-current-timestamp").text(latest.timestamp);
+  };
+
+  var loadData = function(config) {
+    var defaults = {
+      precision: 5
+    };
+
+    config = $.extend({}, defaults, config);
+
+    d3.json(config.apiUrl)
+      .header("x-api-key", config.apiKey)
+      .get(function(error, json) {
+        if (error) {
+          throw error;
+        }
+
+        var datapoints = [];
+        json.Datapoints.forEach(function(d) {
+          datapoints.push({
+            reading: d.Average,
+            timestamp: d3.time.format.iso.parse(d.Timestamp)
+          });
+        });
+
+        datapoints.sort(function(a, b) {
+          if (a.timestamp < b.timestamp) {
+            return -1;
+          }
+
+          if (a.timestamp > b.timestamp) {
+            return 1;
+          }
+
+          return 0;
+        });
+
+        renderGraph(datapoints);
+        showSummary(datapoints, config.precision);
+      });
+  };
+
+  var init = function() {
+    $.getJSON("home-dashboard.json", function(config) {
+      var load = function() { loadData(config) };
+
+      load();
+      window.setTimeout(load, 300);
+      $("#reload").on("click", load);
+    });
+  };
+
+  init();
 });
